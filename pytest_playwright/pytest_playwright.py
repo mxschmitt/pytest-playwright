e3e8e08edfd25f185820633c0085d62cab2ba0bb
Max Schmitt <max@schmitt.mx>
2020-07-13 16:44:51 +0200

    cleanup: added linting (#6)

--- pytest_playwright/pytest_playwright.py
+++ pytest_playwright/pytest_playwright.py
@@ -1,27 +1,34 @@
-import pytest
-import playwright
-from _pytest.config.argparsing import Parser
-
+from asyncio.events import AbstractEventLoop
+from typing import Any, Callable, Awaitable, AsyncGenerator, Dict, Generator
 import asyncio
+
+import pytest
+
+import playwright
+from playwright.page import Page
+from playwright.browser import BrowserContext
+from playwright.browser import Browser
 
 
 @pytest.fixture(scope="session")
-def event_loop():
+def event_loop() -> Generator[AbstractEventLoop, None, None]:
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
 
 
 @pytest.fixture(scope="session")
-def launch_arguments():
+def launch_arguments() -> Dict:
     return {}
 
 
 @pytest.fixture(scope="session")
-async def launch_browser(pytestconfig, launch_arguments):
+async def launch_browser(
+    pytestconfig: Any, launch_arguments: Dict
+) -> Callable[..., Awaitable[Browser]]:
     browser_name = pytestconfig.getoption("browser")
 
-    async def launch(**kwargs):
+    async def launch(**kwargs: Dict[Any, Any]) -> Browser:
         return await playwright.browser_types[browser_name].launch(
             **launch_arguments, **kwargs
         )
@@ -30,27 +37,29 @@ async def launch_browser(pytestconfig, launch_arguments):
 
 
 @pytest.fixture(scope="session")
-async def browser(launch_browser):
+async def browser(
+    launch_browser: Callable[[], Awaitable[Browser]]
+) -> AsyncGenerator[Browser, None]:
     browser = await launch_browser()
     yield browser
     await browser.close()
 
 
 @pytest.fixture
-async def context(browser):
+async def context(browser: Browser) -> AsyncGenerator[BrowserContext, None]:
     context = await browser.newContext()
     yield context
     await context.close()
 
 
 @pytest.fixture
-async def page(context):
+async def page(context: BrowserContext) -> AsyncGenerator[Page, None]:
     page = await context.newPage()
     yield page
     await page.close()
 
 
-def pytest_addoption(parser: Parser):
+def pytest_addoption(parser: Any) -> None:
     group = parser.getgroup("playwright", "Playwright")
     group.addoption(
         "--browser",
