5d6d874d79767975d714f56a3bcfd35579248534
Max Schmitt <max@schmitt.mx>
2020-07-13 22:34:04 +0200

    tests: added basic test setup (#22)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,16 @@
+import sys
+import os
+
 pytest_plugins = ["pytester"]
+
+# The testdir fixture which we use to perform unit tests will set the home directory
+# To a temporary directory of the created test. This would result that the browsers will
+# be re-downloaded each time. By setting the pw browser path directory we can prevent that.
+if sys.platform == "darwin":
+    playwright_browser_path = os.path.expanduser("~/Library/Caches/ms-playwright")
+elif sys.platform == "linux":
+    playwright_browser_path = os.path.expanduser("~/.cache/ms-playwright")
+else:
+    raise ValueError("Not supported!")
+
+os.environ["PLAYWRIGHT_BROWSERS_PATH"] = playwright_browser_path
--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -1,23 +1,127 @@
 from typing import Any
 
 
-def test_simple(testdir: Any) -> None:
-    """Make sure that our plugin works."""
-
-    # create a temporary pytest test file
+def test_default(testdir: Any) -> None:
     testdir.makepyfile(
         """
         import pytest
 
         @pytest.mark.asyncio
-        async def test_hello_default(page):
+        async def test_default(page):
             await page.setContent('<span id="foo">bar</span>')
             assert await page.querySelector("#foo")
             print("done")
     """
     )
-
-    # run all tests with pytest
     result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
 
+
+def test_multiple_browsers(testdir: Any) -> None:
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.asyncio
+        async def test_multiple_browsers(page):
+            await page.setContent('<span id="foo">bar</span>')
+            assert await page.querySelector("#foo")
+            print("done")
+    """
+    )
+    result = testdir.runpytest(
+        "--browser", "firefox", "--browser", "firefox", "--browser", "webkit"
+    )
+    result.assert_outcomes(passed=3)
+
+
+def test_context_arguments(testdir: Any) -> None:
+    testdir.makeconftest(
+        """
+        import pytest
+
+        @pytest.fixture(scope="session")
+        def context_arguments(request):
+            return {"userAgent": "foobar"}
+    """
+    )
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.asyncio
+        async def test_context_arguments(page):
+            assert await page.evaluate("window.navigator.userAgent") == "foobar"
+    """
+    )
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_chromium(testdir: Any) -> None:
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.asyncio
+        async def test_is_chromium(page, browser_name, is_chromium, is_firefox, is_webkit):
+            assert browser_name == "chromium"
+            assert is_chromium
+            assert is_firefox is False
+            assert is_webkit is False
+    """
+    )
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_firefox(testdir: Any) -> None:
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.asyncio
+        async def test_is_firefox(page, browser_name, is_chromium, is_firefox, is_webkit):
+            assert browser_name == "firefox"
+            assert is_chromium is False
+            assert is_firefox
+            assert is_webkit is False
+    """
+    )
+    result = testdir.runpytest("--browser", "firefox")
+    result.assert_outcomes(passed=1)
+
+
+def test_webkit(testdir: Any) -> None:
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.asyncio
+        async def test_is_webkit(page, browser_name, is_chromium, is_firefox, is_webkit):
+            assert browser_name == "webkit"
+            assert is_chromium is False
+            assert is_firefox is False
+            assert is_webkit
+    """
+    )
+    result = testdir.runpytest("--browser", "webkit")
+    result.assert_outcomes(passed=1)
+
+
+def test_goto(testdir: Any) -> None:
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.asyncio
+        async def test_base_url(page, base_url):
+            assert base_url == "https://example.com"
+            await page.goto("/foobar")
+            assert page.url == "https://example.com/foobar"
+            await page.goto("https://www.google.com")
+            assert page.url == "https://www.google.com/"
+    """
+    )
+    result = testdir.runpytest("--base-url", "https://example.com")
     result.assert_outcomes(passed=1)
